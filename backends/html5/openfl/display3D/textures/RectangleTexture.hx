177d609b692a65f21866234fdc0ba242df88f16e
vroad <xvroad@gmail.com>
2014-09-22 16:23:00 +0900

    Add uploadTextureFromUInt8Array on html5 target

--- backends/html5/openfl/display3D/textures/RectangleTexture.hx
+++ backends/html5/openfl/display3D/textures/RectangleTexture.hx
@@ -29,19 +29,10 @@ class RectangleTexture extends TextureBase
         var p = bitmapData.getPixels(new openfl.geom.Rectangle(0, 0, bitmapData.width, bitmapData.height));
         width = bitmapData.width;
         height = bitmapData.height;
-        uploadFromByteArray(p, 0);
+        uploadFromUInt8Array(p.byteView);
     }
 
     public function uploadFromByteArray(data:ByteArray, byteArrayOffset:Int):Void {
-        GL.bindTexture (GL.TEXTURE_2D, glTexture);
-
-        if (optimizeForRenderToTexture)
-            GL.pixelStorei(GL.UNPACK_FLIP_Y_WEBGL, 1); 
-        GL.texParameteri(GL.TEXTURE_2D, GL.TEXTURE_MAG_FILTER, GL.NEAREST);
-        GL.texParameteri(GL.TEXTURE_2D, GL.TEXTURE_MIN_FILTER, GL.NEAREST); 			
-        GL.texParameteri(GL.TEXTURE_2D, GL.TEXTURE_WRAP_S, GL.CLAMP_TO_EDGE);
-        GL.texParameteri(GL.TEXTURE_2D, GL.TEXTURE_WRAP_T, GL.CLAMP_TO_EDGE);   
-
         var source = new UInt8Array(data.length);
         data.position = byteArrayOffset;
         var i:Int = 0;
@@ -50,8 +41,22 @@ class RectangleTexture extends TextureBase
             i++;
         }
 
+        uploadFromUInt8Array(source);
+    }
+    
+    public function uploadFromUInt8Array(data:UInt8Array)
+    {
+        GL.bindTexture (GL.TEXTURE_2D, glTexture);
+
+        if (optimizeForRenderToTexture)
+            GL.pixelStorei(GL.UNPACK_FLIP_Y_WEBGL, 1); 
+        GL.texParameteri(GL.TEXTURE_2D, GL.TEXTURE_MAG_FILTER, GL.NEAREST);
+        GL.texParameteri(GL.TEXTURE_2D, GL.TEXTURE_MIN_FILTER, GL.NEAREST); 			
+        GL.texParameteri(GL.TEXTURE_2D, GL.TEXTURE_WRAP_S, GL.CLAMP_TO_EDGE);
+        GL.texParameteri(GL.TEXTURE_2D, GL.TEXTURE_WRAP_T, GL.CLAMP_TO_EDGE);
+
         // mipLevel always should be 0 in rectangle textures
-        GL.texImage2D( GL.TEXTURE_2D, 0, GL.RGBA, width, height, 0, GL.RGBA, GL.UNSIGNED_BYTE, source );
-        GL.bindTexture (GL.TEXTURE_2D, null);
+        GL.texImage2D( GL.TEXTURE_2D, 0, GL.RGBA, width, height, 0, GL.RGBA, GL.UNSIGNED_BYTE, data );
+        GL.bindTexture (GL.TEXTURE_2D, null);   
     }
 }
